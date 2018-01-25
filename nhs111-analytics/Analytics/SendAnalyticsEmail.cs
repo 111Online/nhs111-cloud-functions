ce25d3f1ec5f28f7148c64637fd55d60b02291ef
Patrick Hughes <patrick.hughes1@nhs.net>
2018-01-25 13:55:46 +0000

    added request trigger for email send

--- nhs111-analytics/Analytics/SendAnalyticsEmail.cs
+++ nhs111-analytics/Analytics/SendAnalyticsEmail.cs
@@ -1,65 +1,38 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Exchange.WebServices.Data;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using NHS111.Cloud.Functions.Models.Email;
+using Task = System.Threading.Tasks.Task;
 
 namespace NHS111.Cloud.Functions.Analytics
 {
     public static class SendAnalyticsEmail
     {
         [FunctionName("SendAnalyticsEmail")]
-        public static void Run([BlobTrigger("analytics/{name}.csv", Connection = "AzureContainerConnection")]CloudBlockBlob analyticsBlob, string name, TraceWriter log)
+        public static async Task Run([BlobTrigger("analytics/{name}.csv", Connection = "AzureContainerConnection")]CloudBlockBlob analyticsBlob, [OrchestrationClient]DurableOrchestrationClient starter, string name, TraceWriter log)
         {
             log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {analyticsBlob.StreamWriteSizeInBytes} Bytes");
             log.Info($"Email for recipients : {analyticsBlob.Metadata["emailrecipients"]}");
 
-            var username = ConfigurationManager.AppSettings["Office365Username"];
-            var password = ConfigurationManager.AppSettings["Office365Password"];
-
-            var credentials = new WebCredentials(username, password);
             var date = name.Substring(name.IndexOf('-') + 1);
-            SendEmail(credentials, ConfigurationManager.AppSettings["EmailFromAddress"], analyticsBlob.Metadata["emailrecipients"], name, date, analyticsBlob);
-        }
+            var subject = $"Data extract for {date} has been created";
+            log.Info($"ToEmailRecipients={analyticsBlob.Metadata["emailrecipients"]}, Subject={subject}");
 
-        private static async void SendEmail(ExchangeCredentials credentials, string fromAddress, string recipients, string filename, string date, CloudBlob analyticsBlob)
-        {
-            var service = new ExchangeService
+            var sendMail = new SendMail
             {
-                Credentials = credentials
+                ToEmails = analyticsBlob.Metadata["emailrecipients"].Split(';'),
+                Subject = subject,
+                Body = $"<h1>Data generated at {DateTime.Now:dd/MM/yyyy hh:mm:ss}</h1>",
+                Attachments = new[] { new KeyValuePair<string, Stream>($"{name}.csv", await analyticsBlob.OpenReadAsync()) }
             };
-
-            service.AutodiscoverUrl(fromAddress, RedirectionUrlValidationCallback);
-
-            var message = new EmailMessage(service)
-            {
-                From = fromAddress,
-                Subject = $"Data extract for {date} has been created",
-                Body = new MessageBody(BodyType.HTML, $"<h1>Data generated at {DateTime.Now:dd/MM/yyyy hh:mm:ss}</h1>")
-            };
-            message.Attachments.AddFileAttachment($"{filename}.csv", await analyticsBlob.OpenReadAsync());
-            
-            foreach (var recipient in recipients.Split(';'))
-                 message.ToRecipients.Add(recipient);
-
-            message.Send();
-        }
-
-        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
-        {
-            // The default for the validation callback is to reject the URL.
-            var result = false;
-            var redirectionUri = new Uri(redirectionUrl);
-
-            // Validate the contents of the redirection URL. In this simple validation
-            // callback, the redirection URL is considered valid if it is using HTTPS
-            // to encrypt the authentication credentials. 
-            if (redirectionUri.Scheme == "https")
-            {
-                result = true;
-            }
-            return result;
+            var instanceId = await starter.StartNewAsync("NHS111OnlineMailSender", JsonConvert.SerializeObject(sendMail));
+            log.Info($"Started orchestration with ID = '{instanceId}'.");
         }
     }
 }
---
+++ nhs111-analytics/Email/RequestSendEmail.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using NHS111.Cloud.Functions.Models.Email;
+
+namespace NHS111.Cloud.Functions.Email
+{
+    public static class RequestSendEmail
+    {
+        [FunctionName("RequestSendEmail")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post")]HttpRequestMessage req, [OrchestrationClient]DurableOrchestrationClient starter, TraceWriter log)
+        {
+            log.Info("C# RequestSendEmail trigger function processed a request.");
+            var sendMail = await req.Content.ReadAsAsync<SendMail>();
+
+            log.Info($"ToEmailRecipients={string.Join(";", sendMail.ToEmails.Select(e => e.ToString()).ToArray())}, Subject={sendMail.Subject}");
+            var instanceId = await starter.StartNewAsync("NHS111OnlineMailSender", JsonConvert.SerializeObject(sendMail));
+            log.Info($"Started orchestration with ID = '{instanceId}'.");
+
+            return req.CreateResponse(HttpStatusCode.OK, instanceId);
+        }
+    }
+}
