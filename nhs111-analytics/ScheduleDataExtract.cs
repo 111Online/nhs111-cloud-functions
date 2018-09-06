ec95e480dfe396e911e22680d17c721f1c57fb36
Patrick Hughes <patrick.hughes1@nhs.net>
2018-09-06 16:52:56 +0100

    added alert for failed data sebd

--- nhs111-analytics/ScheduleDataExtract.cs
+++ nhs111-analytics/ScheduleDataExtract.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
@@ -6,7 +7,9 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage.Table;
 using Newtonsoft.Json;
 using NHS111.Cloud.Functions.Analytics;
+using NHS111.Cloud.Functions.Email;
 using NHS111.Cloud.Functions.Models.Analytics;
+using NHS111.Cloud.Functions.Models.Email;
 
 namespace NHS111.Cloud.Functions
 {
@@ -16,24 +19,45 @@ namespace NHS111.Cloud.Functions
         public static async Task Run([TimerTrigger("0 0 6 * * *")]TimerInfo timer, [Table("AnalyticsEmailTable", "Email", Connection = "AzureContainerConnection")]IQueryable<AnalyticsEmail> analyticsEmails, [Table("AnalyticsEmailTable", "Email", Connection = "AzureContainerConnection")]CloudTable outTable, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            log.Info($"Checking data import has completed successfully for {DateTime.Now.AddDays(-1).Date.ToShortDateString()}");
+            //
+            //some code
+            //log error (no it hasn't)
+            
             foreach (var analyticsEmail in analyticsEmails)
             {
+                //set the initial end date
                 var endDate = Convert.ToDateTime(analyticsEmail.StartDate).AddDays(analyticsEmail.NumberOfDays);
-                if (endDate.Date > DateTime.Now.Date) continue;
-                log.Info($"StpList={analyticsEmail.StpList}, CcgList={analyticsEmail.CcgList}, ToEmailRecipients={analyticsEmail.ToEmailRecipients}, StartDate={analyticsEmail.StartDate}");
-                try
+                while (DateTime.Now.Date > endDate.Date)
                 {
-                    OrchestrateDailyDataSend.Run(JsonConvert.SerializeObject(analyticsEmail), log);
-                    var updateAnalyticsEmail = await GetTableEntity(outTable, analyticsEmail.PartitionKey, analyticsEmail.RowKey);
-                    updateAnalyticsEmail.StartDate = Convert.ToDateTime(analyticsEmail.StartDate).AddDays(analyticsEmail.NumberOfDays).ToString("yyyy-MM-dd");
-                    await UpdateTableEntity(outTable, updateAnalyticsEmail);
-                }
-                catch (Exception e)
-                {
-                    log.Error("An error occurred", e);
-                    log.Error($"Error detail - {e.Message}", e);
-                    if(e.InnerException != null) log.Error($" --> inner Error detail - {e.InnerException}", e.InnerException);
-                    throw;
+                    log.Info($"StpList={analyticsEmail.StpList}, CcgList={analyticsEmail.CcgList}, ToEmailRecipients={analyticsEmail.ToEmailRecipients}, StartDate={analyticsEmail.StartDate}");
+                    try
+                    {
+                        OrchestrateDailyDataSend.Run(JsonConvert.SerializeObject(analyticsEmail), log);
+                        var updateAnalyticsEmail = await GetTableEntity(outTable, analyticsEmail.PartitionKey, analyticsEmail.RowKey);
+                        updateAnalyticsEmail.StartDate = Convert.ToDateTime(analyticsEmail.StartDate).AddDays(analyticsEmail.NumberOfDays).ToString("yyyy-MM-dd");
+                        await UpdateTableEntity(outTable, updateAnalyticsEmail);
+                        //set end data again to see if we need to send again
+                        endDate = endDate.AddDays(analyticsEmail.NumberOfDays);
+                    }
+                    catch (Exception e)
+                    {
+                        log.Error("An error occurred", e);
+                        log.Error($"Error detail - {e.Message}", e);
+                        if (e.InnerException != null) log.Error($" --> inner Error detail - {e.InnerException}", e.InnerException);
+
+                        //send email to slack channel to raise error
+                        var sendMail = new SendMail
+                        {
+                            ToEmails = new[] { ConfigurationManager.AppSettings["SlackChannelMailAddress"] },
+                            EmailType = "DataExtract",
+                            Body = $"Error detail - {e.Message}",// {e.InnerException!= null ? $"--> inner Error detail - {e.InnerException}" : $"{string.Empty}" },
+                            Subject = "Data extract sending function failure"
+                        };
+                        await SendEmail.Run(JsonConvert.SerializeObject(sendMail), log);
+                        throw;
+                    }
                 }
             }  
         }
